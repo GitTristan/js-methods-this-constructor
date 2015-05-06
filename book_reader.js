d14388c61daf4a1691c9d25ca3529b4a8a5c73b8
Tristan Rubadeau <tristan@rubadeau.com>
2015-05-05 21:27:57 -0600

    BookReader complete

--- book_reader.js
+++ book_reader.js
@@ -1,21 +1,32 @@
 function BookReader(book) {
-  // your code here
+  this.book = book;
+  this.currentPage = 0;
 }
 
 BookReader.prototype.nextPage = function() {
-  // your code here
+  if (this.currentPage < this.book.length - 1) {
+    this.currentPage = this.currentPage + 1;
+  }
+  return this.book[this.currentPage]
 };
 
 BookReader.prototype.previousPage = function() {
-  // your code here
+  if (this.currentPage > 0){
+    this.currentPage = this.currentPage - 1;
+  }
+  return this.book[this.currentPage];
 };
 
 BookReader.prototype.pagesLeft = function() {
-  // your code here
+  return (this.book.length - 1) - this.currentPage
 };
 
 BookReader.prototype.encouragement = function() {
-  // your code here
+  if (this.pagesLeft() > 1) {
+    return "Keep going, only " + this.pagesLeft() + " pages left after this one!";
+  } else {
+    return "Keep going, this book is good 'til the last drop!";
+  }
 };
 
 // DO NOT MODIFY BELOW THIS COMMENT:
