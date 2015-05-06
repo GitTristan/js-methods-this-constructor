460e53dbaddf4757c694a6e2c04b2efcd4a0e920
Tristan Rubadeau <tristan@rubadeau.com>
2015-05-05 21:12:08 -0600

    completed Taxi problems

--- taxi.js
+++ taxi.js
@@ -1,13 +1,29 @@
 function Taxi(driverName, maxNumberOfPassengers) {
   this.driverName = driverName;
   this.maxNumberOfPassengers = maxNumberOfPassengers;
+  this.passengers = [];
 }
 
 Taxi.prototype.addPassenger = function(passengerName) {
-  // your code here
+  this.passengers.push(passengerName);
 };
 
-// your code here
+Taxi.prototype.passengerCount = function(nthpassenger) {
+  var array = this.passengers.push(this.passengerName);
+  return array - 1;
+};
 
+Taxi.prototype.full = function() {
+  if (this.passengers.length === this.maxNumberOfPassengers) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Taxi.prototype.dropOffPassengers = function() {
+  this.passengers.splice(0, this.passengers.length);
+  return this.passengers;
+};
 // DO NOT MODIFY BELOW THIS COMMENT:
 module.exports = Taxi;
